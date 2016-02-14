d7ebe24cdd2adbfb2e12784f7804cb9060dff308
Alexandr <Elizarov>
2016-02-15 00:34:53 +0300

    Теперь уже пользовательский код не должен прописывать все вручную, а должен просто вызвать нужный метод тестового класса (а тестовый класс уже написан с использованием моей архитектуры). Но пока что тесть компилируется, но не срабатывает (архитектура то еще сырая)

--- src/myJUnit/TestMyJunitFramework.java
+++ src/myJUnit/TestMyJunitFramework.java
@@ -3,10 +3,7 @@ package myJUnit;
 public class TestMyJunitFramework {
 	
 	public static void main(String[] args){
-		WasRun test = new WasRun();
-		System.out.println(test.getWasRun());
-		test.testMethod();
-		System.out.println(test.getWasRun());
+		new TestCaseTest().testRunning();
 	}
 
 }

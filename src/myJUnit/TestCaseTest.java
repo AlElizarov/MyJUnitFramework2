23f614a5db2400cad5696578a4a18837a33c4f42
Alexandr <Elizarov>
2016-02-15 00:30:14 +0300

    На самом деле, мы опять тестируем запуск теста, то есть флаг wasRun. Единтсвенное, что мы тестируем у TestCase это метод run. Метод run должен быть именно в TestCase, а объект мы создаем WasRun, так что его придется наследовать от TestCase. Но это наследование не создает теста, это просто наследование в моем дизайне

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -1,5 +1,12 @@
 package myJUnit;
 
 public class TestCaseTest extends MyTestCase {
+	
+	public void testRunning(){
+		WasRun test = new WasRun();
+		System.out.println(test.getWasRun());
+		test.testMethod();
+		System.out.println(test.getWasRun());
+	}
 
 }

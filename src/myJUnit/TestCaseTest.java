cde94553b87f1144f7b0c5eff2caad27396dec24
Alexandr <Elizarov>
2016-02-15 00:32:09 +0300

    поменял testMethod на run

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -5,7 +5,7 @@ public class TestCaseTest extends MyTestCase {
 	public void testRunning(){
 		WasRun test = new WasRun();
 		System.out.println(test.getWasRun());
-		test.testMethod();
+		test.run();
 		System.out.println(test.getWasRun());
 	}
 

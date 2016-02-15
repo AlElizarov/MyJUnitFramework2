03959650c27a07774f018dc5eaeb3b4d6250ff18
Alexandr <Elizarov>
2016-02-15 15:53:48 +0300

    Попытался воспользоваться своей инфраструктуройб в частности методом setUp(). Но у меня не получилось. Потому что у TestCaseTest то я не вызываю run(). А сердце инфраструктуры - именно метод run(). Он все вызывает правильно и в правильном порядке. Надо переходить к свое задумке: просто создавать объект класса TestCaseTest и вызывать у него run(). Тогда надо будет допилить метод run  так, что бы он вызывал все тестовые методы тестового класса

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -2,21 +2,24 @@ package myJUnit;
 
 public class TestCaseTest extends MyTestCase {
 	
+	WasRun test;
+	
+	@Override
+	public void setUp(){
+		test = new WasRun();
+	}
+	
 	public void testRunning(){
-		WasRun test = new WasRun();
-		assert(!test.getWasRun());
 		test.run();
 		assert(test.getWasRun());
 	}
 	
 	public void testSetUp(){
-		WasRun test = new WasRun();
 		test.run();
 		assert(test.wasSetup());
 	}
 	
 	public void testRef(){
-		WasRun test = new WasRun();
 		test.run();
 		assert(2 == test.getCountMethods());
 	}

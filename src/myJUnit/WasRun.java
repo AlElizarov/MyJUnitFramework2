a28c1e07d838870e09e8c321da10458ee949f9a0
Alexandr <Elizarov>
2016-02-15 16:19:56 +0300

    Все флаги удалены и теперь вся структура задается журналом

--- src/myJUnit/WasRun.java
+++ src/myJUnit/WasRun.java
@@ -2,20 +2,13 @@ package myJUnit;
 
 public class WasRun extends MyTestCase{
 
-	private boolean wasRun;
 	private String log;
-	
-	public boolean getWasRun() {
-		return wasRun;
-	}
 
 	public void testMethod() {
 		log += "testMethod ";
-		wasRun = true;
 	}
 
 	public void setUp() {
-		wasRun = false;
 		log = "setUp ";
 	}
 

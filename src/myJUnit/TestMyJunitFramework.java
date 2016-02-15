846010123aaf8da0173c7aa21d759d0f038689a4
Alexandr <Elizarov>
2016-02-15 09:04:28 +0300

    Сразу заставил тест и компилироваться и проходить. Увеличиваем шаги

--- src/myJUnit/TestMyJunitFramework.java
+++ src/myJUnit/TestMyJunitFramework.java
@@ -3,7 +3,9 @@ package myJUnit;
 public class TestMyJunitFramework {
 	
 	public static void main(String[] args){
-		new TestCaseTest().testRunning();
+		TestCaseTest test = new TestCaseTest();
+		test.testRunning();
+		test.testSetUp();
 	}
 
 }
--- src/myJUnit/WasRun.java
+++ src/myJUnit/WasRun.java
@@ -3,6 +3,7 @@ package myJUnit;
 public class WasRun extends MyTestCase{
 
 	private boolean wasRun;
+	private boolean wasSetup;
 	
 	public boolean getWasRun() {
 		return wasRun;
@@ -10,6 +11,14 @@ public class WasRun extends MyTestCase{
 
 	public void testMethod() {
 		wasRun = true;
+	}
+
+	public void setUp() {
+		wasSetup = true;
+	}
+
+	public boolean wasSetup() {
+		return wasSetup;
 	}
 
 }

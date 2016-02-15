ffaabec6641c1468055239309e338c67880b7ba0
Alexandr <Elizarov>
2016-02-15 18:33:03 +0300

    Возвращаюсь к идее различать в тестах ошибки и исключения

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -4,7 +4,7 @@ public class TestCaseTest extends MyTestCase {
 	
 	public void testResult(){
 		WasRun test = new WasRun();
-		assert ("2 run, 1 failed".equals(test.run()));
+		assert ("2 run, 1 failed, 0 failures".equals(test.run()));
 	}
 
 }

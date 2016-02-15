046b502ff30c46416250b019563530a04bdc2764
Alexandr <Elizarov>
2016-02-15 18:30:48 +0300

    Мы удалили все что отвечало за порядок выполнения методов setUp и tearDown, потому что там возникает сложности с Java reflection API, с которыми я не должен бороться

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -1,21 +1,15 @@
 package myJUnit;
 
 public class TestCaseTest extends MyTestCase {
-
-	public void testSetUp() {
-		WasRun test = new WasRun();
-		test.run();
-		assert ("setUp testMethod tearDown ".equals(test.getLog()));
-	}
 	
 	public void testResult(){
 		WasRun test = new WasRun();
-		assert ("2 run, 1 failed, 0 failure".equals(test.run()));
+		assert ("2 run, 1 failed".equals(test.run()));
 	}
 	
 	public void testFailed(){
 		WasRun test = new WasRun();
-		assert ("2 run, 1 failed, 0 failure".equals(test.run()));
+		assert ("2 run, 1 failed".equals(test.run()));
 	}
 
 }
--- src/myJUnit/WasRun.java
+++ src/myJUnit/WasRun.java
@@ -2,24 +2,9 @@ package myJUnit;
 
 public class WasRun extends MyTestCase{
 
-	private String log;
-
 	public void testMethod() {
-		log += "testMethod ";
 	}
 
-	public void setUp() {
-		log = "setUp ";
-	}
-
-	public String getLog() {
-		return log;
-	}
-	
-	public void tearDown(){
-		log += "tearDown ";
-	}
-	
 	public void testBroken(){
 		throw new RuntimeException();
 	}

20e55cc08907c1ac8ffa81e3359d728f27805a57
Alexandr <Elizarov>
2016-02-15 15:55:47 +0300

    Будем опять двигаться маленькими шагами: вернулись к тестированию WasRun(а это тестовый класс). У него только один тестовый метод. Поэтому, если я сейчас заменю его явный вызов на цикл - ничего не должно сломаться

--- src/myJUnit/TestCaseTest.java
+++ src/myJUnit/TestCaseTest.java
@@ -2,24 +2,27 @@ package myJUnit;
 
 public class TestCaseTest extends MyTestCase {
 	
-	WasRun test;
+	//WasRun test;
 	
 	@Override
 	public void setUp(){
-		test = new WasRun();
+		//test = new WasRun();
 	}
 	
 	public void testRunning(){
+		WasRun test = new WasRun();
 		test.run();
 		assert(test.getWasRun());
 	}
 	
 	public void testSetUp(){
+		WasRun test = new WasRun();
 		test.run();
 		assert(test.wasSetup());
 	}
 	
 	public void testRef(){
+		WasRun test = new WasRun();
 		test.run();
 		assert(2 == test.getCountMethods());
 	}

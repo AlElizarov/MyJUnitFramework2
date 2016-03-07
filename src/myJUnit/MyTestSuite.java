6696014233833e0716393bfdc4832ffaad374868
Alexandr <Elizarov>
2016-03-07 19:57:01 +0300

    Здесь теперь запускается GUI

--- src/myJUnit/MyTestSuite.java
+++ src/myJUnit/MyTestSuite.java
@@ -2,8 +2,10 @@ package myJUnit;
 
 import java.util.ArrayList;
 
+import javax.swing.SwingUtilities;
+
 public class MyTestSuite {
-	
+
 	private ArrayList<MyTestCase> tests = new ArrayList<>();
 
 	public void add(MyTestCase test) {
@@ -11,11 +13,32 @@ public class MyTestSuite {
 	}
 
 	public TestResult run() {
+		
 		TestResult result = new TestResult();
-		for(int i = 0; i < tests.size(); i++){
+		for (int i = 0; i < tests.size(); i++) {
 			tests.get(i).run().addResults(result);
-			result.getTestClasses().put(tests.get(i).getClass().getSimpleName(), tests.get(i).run().getTestMethods());
+			result.getTestClasses().put(
+					tests.get(i).getClass().getSimpleName(),
+					tests.get(i).run().getTestMethods());
 		}
+		String className = getClass().getName();
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+//					try {
+//						UIManager.setLookAndFeel(UIManager
+//								.getSystemLookAndFeelClassName());
+//					} catch (ClassNotFoundException | InstantiationException
+//							| IllegalAccessException
+//							| UnsupportedLookAndFeelException e) {
+//						e.printStackTrace();
+//					}
+				GuiForMyJUnit gui = new GuiForMyJUnit(result);
+				gui.setSuiteName(className);
+				gui.createAndShowGui();
+			}
+		});
 		return result;
 	}
 
